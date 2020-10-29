3302fa53f0a642e831ab4a38bd22eb01d2617639
irma <irma.ravkic@gmail.com>
2020-10-29 14:16:46 -0700

    final change

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,18 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
-
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    
     def show
         #byebug
     end
 
     def index
-        @articles = Article.all
+        @articles = Article.paginate(page: params[:page], per_page: 2)
     end
 
     def new
-        @article = Article.new #have to add in otder to avoid error in new.html.erb
+        @article = Article.new #have to add i30n otder 30to avoid error in new.html.erb
     end
 
     def edit
@@ -19,7 +21,8 @@ class ArticlesController < ApplicationController
     def create
         art = params[:article]
         @article = Article.new(article_params) #whitelisting
-        @article.user = User.first #hard coding
+        #@article.user = User.first #hard coding
+        @article.user = current_user
         if @article.save
             flash[:notice] = "Article was created successfully."
             redirect_to @article
@@ -55,6 +58,13 @@ class ArticlesController < ApplicationController
 
     def article_params
         params.require(:article).permit(:title, :description) #whitelisting
+    end
+
+    def require_same_user
+        if current_user != @article.user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your own article"
+            redirect_to @article
+        end
     end
         
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
